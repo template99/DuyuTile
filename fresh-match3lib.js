8f31c05add37b09213f4a2d2c642183aa95cc371
Template99 <template99@comcast.net>
2023-01-20 22:06:15 -0600

    last push before merging.

--- fresh-match3lib.js
+++ fresh-match3lib.js
@@ -3,7 +3,7 @@
 // The function gets called when the window is fully loaded
 window.onload = function() {
     // Get the canvas and context
-    console.log('window onload loaded');
+    //console.log('window onload loaded');
     var canvas = document.getElementById("viewport");
     var context = canvas.getContext("2d");
 
